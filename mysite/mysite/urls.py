b7efc86ee85ce7a911a098a7894096149df035e5
Artemiy <artemiy.borodulin@gmail.com>
2023-06-06 15:32:49 +0500

    Account application

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('pages/', include('pages.urls')),
+    path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
 ]
